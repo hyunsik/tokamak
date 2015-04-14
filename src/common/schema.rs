05955d427e6f5a427b0b6114d73ecffc49d8097a
Hyunsik Choi <hyunsik@apache.org>
2015-04-14 04:08:25 -0700

    Add base layout of Vectorized Row Blocks.
    * Improve column and schema.
    * Add SlowVecRowBlock
    * Add VecRowBlockTrait
    * Add RowBlock struct.

--- src/common/schema.rs
+++ src/common/schema.rs
@@ -2,27 +2,42 @@ use common::data_type::TypeClass;
 use common::data_type::DataType;
 
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct Column {
   name: String,
   data_type: DataType,
 }
 
+impl Column {
+  pub fn new(column_name: String, type_class: TypeClass) -> Column {
+    Column {name: column_name, data_type: DataType::new(type_class)}
+  }
+}
+
+#[derive(Clone, PartialEq, Debug)]
 pub struct Schema {
   columns : Vec<Column>
 }
 
 impl Schema {
-  fn add(&mut self, c : Column) {
+  pub fn new(columns: Vec<Column>) -> Schema {
+    Schema {columns: columns}
+  }
+
+  pub fn add(&mut self, c : Column) {
     self.columns.push(c);
   }
 
-  fn get(&self, idx : usize) -> &Column {
+  pub fn add_directly(&mut self, name : String, type_class: TypeClass) {
+    self.columns.push(Column::new(name, type_class));
+  }
+
+  pub fn get(&self, idx : usize) -> &Column {
     debug_assert!(idx < self.columns.len(), "Column index out of range");
     &self.columns[idx]
   }
 
-  fn get_by_name(&self, name : String) -> Option<&Column> {
+  pub fn get_by_name(&self, name : String) -> Option<&Column> {
     self.columns.iter().filter(|&c| c.name == name).next()
   }
 }
--- src/tuple/tuple.rs
+++ src/tuple/tuple.rs
@@ -1,5 +1,52 @@
+use common::Column;
 use common::Schema;
+use common::TypeClass;
 
-trait RowBatch {
+pub struct SlotVecRowBlock {
+  schema: Schema
+}
+
+pub struct AllocatedVecRowBlock {
+  schema: Schema
+}
+
+trait VecRowBlockTrait {
   fn schema(&self) -> Schema;
+}
+
+impl VecRowBlockTrait for SlotVecRowBlock {
+    fn schema(&self) -> Schema {
+        self.schema.clone()
+    }
+}
+
+impl VecRowBlockTrait for AllocatedVecRowBlock {
+    fn schema(&self) -> Schema {
+        self.schema.clone()
+    }
+}
+
+struct RowBlock<R> {
+    rowblock: R
+}
+
+impl<R:VecRowBlockTrait> RowBlock<R> {
+    #[inline(always)]
+    fn schema(&self) -> Schema {
+      self.rowblock.schema()
+    }
+}
+
+#[test]
+fn test_rowblock() {
+  let mut columns = Vec::new();
+  columns.push(Column::new("c1".to_string(), TypeClass::INT4));
+  columns.push(Column::new("c2".to_string(), TypeClass::INT8));
+  columns.push(Column::new("c3".to_string(), TypeClass::FLOAT4));
+
+  let schema = Schema::new(columns);
+  let rowblock = RowBlock {rowblock: SlotVecRowBlock {schema: schema} };
+  //let rowblock.schema();
+
+  //assert!(schema == rowblock.schema());
 }
