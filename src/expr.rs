1ce8e9dad164b73f77e9f949aa8ce19ca35ca623
Hyunsik Choi <hyunsik@apache.org>
2015-07-08 12:21:44 +0900

    Implement main walker for Expr visitor.

--- src/expr.rs
+++ src/expr.rs
@@ -59,7 +59,7 @@ pub struct WinFnDecl {
   signature: String  
 }
 
-pub enum BinCompOp {
+pub enum CompOp {
   Eq,
   NotEq,
   Lth,
@@ -68,7 +68,7 @@ pub enum BinCompOp {
   Geq
 }
 
-pub enum BinArithOp {
+pub enum ArithmOp {
   Plus,
   Minus,
   Multiply,
@@ -84,25 +84,25 @@ pub struct Expr {
 pub enum ExprSpec {
   // Unary Expressions
   Not(Box<Expr>),
-  IsNull(Box<Expr>,Box<Expr>, bool), // bool - 'is not null' if true
-  Signed(Box<Expr>),
+  IsNull(Box<Expr>, bool), // bool - 'is null' if true. 'is not null' if false.
+  Sign(Box<Expr>, bool), // true - Plus, false - Minus
 
   // Binary Arithmetic Expressions
   And(Box<Expr>,Box<Expr>),
   Or(Box<Expr>,Box<Expr>),
-  Comp(BinCompOp, Box<Expr>,Box<Expr>),
-  BinArithmetic(BinArithOp,Box<Expr>,Box<Expr>),
-  Concatenate(Box<Expr>,Box<Expr>),
+  Comp(CompOp, Box<Expr>,Box<Expr>),
+  Arithm(ArithmOp,Box<Expr>,Box<Expr>),
+  Concat(Box<Expr>,Box<Expr>),
 
   // Functions
-  Function(Box<FnDecl>, Vec<Box<Expr>>),
-  AggFunction(Box<AggFnDecl>, Vec<Box<Expr>>),
-  WindowFunction(Box<WinFnDecl>, Vec<Box<Expr>>),
+  Fn(Box<FnDecl>, Vec<Box<Expr>>),
+  AggFn(Box<AggFnDecl>, Vec<Box<Expr>>),
+  WinFn(Box<WinFnDecl>, Vec<Box<Expr>>),
 
   // String operators or pattern matching predicates: lhs - pattern, rhs - value  
-  Like(Box<Expr>,Box<Expr>), 
-  SimilarTo(Box<Expr>,Box<Expr>),
-  RegexMatch(Box<Expr>,Box<Expr>),  
+  Like(Box<Expr>,Box<Expr>, bool), 
+  SimilarTo(Box<Expr>,Box<Expr>, bool),
+  RegexMatch(Box<Expr>,Box<Expr>, bool),  
 
   // Other predicates
   Between(Box<Expr>,Box<Expr>,Box<Expr>), // predicand, begin, end
@@ -122,30 +122,125 @@ pub enum ExprSpec {
 pub trait Visitor<'v>: Sized {
   fn visit_not(&mut self, child: &'v Expr);  
   fn visit_is_null(&mut self, child: &'v Expr, not: bool);
+  fn visit_sign(&mut self, child: &'v Expr, not: bool);
 
   fn visit_and(&mut self, lhs: &'v Expr, rhs: &'v Expr);
   fn visit_or(&mut self, lhs: &'v Expr, rhs: &'v Expr);
-  fn visit_comp(&mut self, lhs: &'v Expr, rhs: &'v Expr);
-  fn visit_bin_arith_op(&mut self, op: &BinArithOp, lhs: &'v Expr, rhs: &'v Expr);
+  fn visit_comp(&mut self, op: &CompOp, lhs: &'v Expr, rhs: &'v Expr);
+  fn visit_arithm(&mut self, op: &ArithmOp, 
+    lhs: &'v Expr, rhs: &'v Expr);
   fn visit_concat(&mut self, lhs: &'v Expr, rhs: &'v Expr);
 
-  fn visit_fn(&mut self, fd: &'v FnDecl, args: &'v Vec<Expr>);
-  fn visit_aggfn(&mut self, aggfd: &'v AggFnDecl, args: &'v Vec<Expr>);
-  fn visit_winfn(&mut self, winfd: &'v WinFnDecl, args: &'v Vec<Expr>);
+  fn visit_fn(&mut self, fd: &'v FnDecl, args: &'v Vec<Box<Expr>>);
+  fn visit_aggfn(&mut self, aggfd: &'v AggFnDecl, args: &'v Vec<Box<Expr>>);
+  fn visit_winfn(&mut self, winfd: &'v WinFnDecl, args: &'v Vec<Box<Expr>>);
 
-  fn visit_like(&mut self, pattern: &'v Expr, pred: &'v Expr, not: bool);
-  fn visit_similarto(&mut self, pattern: &'v Expr, pred: &'v Expr, not: bool);
-  fn visit_regexmatch(&mut self, pattern: &'v Expr, pred: &'v Expr, not: bool);
+  fn visit_like(&mut self, pat: &'v Expr, pred: &'v Expr, not: bool);
+  fn visit_similarto(&mut self, pat: &'v Expr, pred: &'v Expr, not: bool);
+  fn visit_regexmatch(&mut self, pat: &'v Expr, pred: &'v Expr, not: bool);
 
   fn visit_between(&mut self, pred: &'v Expr, begin: &'v Expr, end: &'v Expr);
-  fn visit_in(&mut self, pred: &'v Expr, &'v Vec<Expr>);
+  fn visit_in(&mut self, pred: &'v Expr, &'v Vec<Box<Expr>>);
 
   fn visit_case(&mut self, ifconds: &'v Vec<Box<Expr>>, else_result: &'v Expr);
   fn visit_if_then(&mut self, ifcond: &'v Expr, result: &'v Expr);
 
-  fn visit_cast(&mut self, expr: &'v Expr, from: &'v DataType, to: &'v DataType);
+  fn visit_cast(&mut self, expr: &'v Expr, from: &'v DataType, 
+    to: &'v DataType);
   fn visit_row(&mut self, row: &'v Vec<Box<Expr>>);
-
   fn visit_field(&mut self, field: &'v Column);
   fn visit_const(&mut self, datum: &'v Datum);
+}
+
+pub fn walk_expr<'v, V: Visitor<'v>>(visitor: &mut V, expr: &'v Expr) {
+
+  match expr.node {
+
+    ExprSpec::Not(ref child) => {
+      visitor.visit_not(child);
+    }
+
+    ExprSpec::IsNull(ref child, positive) => {
+      visitor.visit_is_null(child, positive);
+    }
+
+    ExprSpec::Sign(ref child, positive) => {
+      visitor.visit_sign(child, positive);
+    }
+
+    ExprSpec::And(ref lhs, ref rhs) => {
+      visitor.visit_and(lhs, rhs);
+    }
+
+    ExprSpec::Or(ref lhs, ref rhs) => {
+      visitor.visit_or(lhs, rhs);
+    }
+
+    ExprSpec::Comp(ref op, ref lhs, ref rhs) => {
+      visitor.visit_comp(op, lhs, rhs);
+    }
+
+    ExprSpec::Arithm(ref op, ref lhs, ref rhs) => {
+      visitor.visit_arithm(op, lhs, rhs);
+    }
+
+    ExprSpec::Concat(ref lhs, ref rhs) => {
+      visitor.visit_concat(lhs, rhs);
+    }
+
+
+    ExprSpec::Fn(ref fd, ref args) => {
+      visitor.visit_fn(fd, args);
+    },
+    
+    ExprSpec::AggFn(ref fd, ref args) => {
+      visitor.visit_aggfn(fd, args);
+    },
+
+    ExprSpec::WinFn(ref fd, ref args) => {
+      visitor.visit_winfn(fd, args);
+    },
+
+
+    ExprSpec::Like(ref pat, ref pred, not) => {
+      visitor.visit_like(pat, pred, not)
+    },
+
+    ExprSpec::SimilarTo(ref pat, ref pred, not) => {
+      visitor.visit_similarto(pat, pred, not)
+    },
+
+    ExprSpec::RegexMatch(ref pat, ref pred, not) => {
+      visitor.visit_regexmatch(pat, pred, not)
+    },
+
+
+    ExprSpec::Between(ref pred, ref begin, ref end) => {
+      visitor.visit_between(pred, begin, end)
+    }
+
+    ExprSpec::In(ref pred, ref values) => {
+      visitor.visit_in(pred, values)
+    }
+
+
+    ExprSpec::Case(ref ifconds, ref else_result) => {
+      visitor.visit_case(ifconds, else_result)
+    }
+
+    ExprSpec::IfThen(ref ifcond, ref result) => {
+      visitor.visit_if_then(ifcond, result)
+    },
+
+
+    ExprSpec::Cast(ref value, ref from, ref to) => {
+      visitor.visit_cast(value, from, to)
+    },
+
+    ExprSpec::Row(ref values) => visitor.visit_row(values),
+
+    ExprSpec::Field(ref column) => visitor.visit_field(column),
+
+    ExprSpec::Const(ref datum) => visitor.visit_const(datum),
+  }
 }
