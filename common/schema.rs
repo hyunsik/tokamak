9b94d8d71d1c626aba36edb8f0d415c46fa554eb
Hyunsik Choi <hyunsik@apache.org>
2015-09-27 00:34:35 -0700

    Changed coding converntion.

--- common/schema.rs
+++ common/schema.rs
@@ -11,28 +11,32 @@ pub type ColumnId = usize;
 
 
 #[derive(Clone, PartialEq, Debug)]
-pub struct Constraint {
+pub struct Constraint 
+{
   pub nullable: bool,
   pub unique: bool,
   pub sorted: bool
 }
 
 
-pub struct Identifier {
+pub struct Identifier 
+{
   pub name: String,
   pub quoted: bool
 }
 
-impl Identifier {
-  
-  pub fn new<T: AsRef<str>>(name: T) -> Identifier {
+impl Identifier 
+{
+  pub fn new<T: AsRef<str>>(name: T) -> Identifier 
+  {
     Identifier {
       name: name.as_ref().to_string(), 
       quoted: false
     }
   }
     
-  pub fn new_quoted<T: AsRef<str>>(name: T, quoted: bool) -> Identifier {
+  pub fn new_quoted<T: AsRef<str>>(name: T, quoted: bool) -> Identifier 
+  {
     Identifier {
       name: name.as_ref().to_string(), 
       quoted: quoted
@@ -42,15 +46,17 @@ impl Identifier {
 
 
 #[derive(Clone, PartialEq, Debug)]
-pub struct Field {
+pub struct Field 
+{
   pub name: String,
   pub decl: FieldDecl,
   pub cstr: Option<Constraint> 
 }
 
-impl Field {
-  
-  pub fn new<T: AsRef<str>>(name: T, decl: FieldDecl) -> Field {
+impl Field 
+{
+  pub fn new<T: AsRef<str>>(name: T, decl: FieldDecl) -> Field 
+  {
     Field {
       name: name.as_ref().to_string(),
       decl: decl,
@@ -58,76 +64,83 @@ impl Field {
     }
   }
   
-  pub fn scalar<T: AsRef<str>>(name: T, ty: Ty) -> Field {
+  pub fn scalar<T: AsRef<str>>(name: T, ty: Ty) -> Field 
+  {
     Field::new(name, FieldDecl::Scalar(ty))
   }
   
-  pub fn array<T: AsRef<str>>(name: T, decl: FieldDecl) -> Field {
+  pub fn array<T: AsRef<str>>(name: T, decl: FieldDecl) -> Field 
+  {
     Field::new(name, FieldDecl::Array(Box::new(decl)))
   }
   
-  pub fn map<T: AsRef<str>>(name: T, key_ty: FieldDecl, val_ty: FieldDecl) -> Field {
+  pub fn map<T: AsRef<str>>(name: T, key_ty: FieldDecl, val_ty: FieldDecl) -> Field 
+  {
     Field::new(name, FieldDecl::Map(Box::new(key_ty), Box::new(val_ty)))
   }
   
-  pub fn record<T: AsRef<str>>(name: T, r: Record) -> Field {
+  pub fn record<T: AsRef<str>>(name: T, r: Record) -> Field 
+  {
     Field::new(name, FieldDecl::Record(r))
   }
   
-  pub fn record_from_vec<T: AsRef<str>>(name: T, fields: Vec<Field>) -> Field {
+  pub fn record_from_vec<T: AsRef<str>>(name: T, fields: Vec<Field>) -> Field 
+  {
     Field::new(name, FieldDecl::Record(Record::new(fields)))
   }
   
   #[inline]
-  pub fn name(&self) -> &str {
-    &self.name
-  }
+  pub fn name(&self) -> &str { &self.name }
 }
 
-impl fmt::Display for Field {
-  
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+
+impl fmt::Display for Field 
+{
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result 
+  {
     write!(f, "{}", display_field(self))
   }
 }
 
 #[inline]
-fn display_field(f: &Field) -> String {
-  format!("{} {}", f.name, f.decl)
-}
+fn display_field(f: &Field) -> String { format!("{} {}", f.name, f.decl) }
 
 #[inline]
-fn display_fields(fields: &Vec<Field>) -> String {
+fn display_fields(fields: &Vec<Field>) -> String 
+{
   fields.iter().map(|f| display_field(f)).join(", ")
 }
 
 
 #[derive(Clone, PartialEq, Debug)]
-pub enum FieldDecl {
+pub enum FieldDecl 
+{
   Scalar (Ty),
   Record (Record),
   Array  (Box<FieldDecl>),
   Map    (Box<FieldDecl>, Box<FieldDecl>)
 }
 
-impl FieldDecl {
+impl FieldDecl 
+{
+  pub fn scalar(ty: Ty) -> FieldDecl { FieldDecl::Scalar(ty) }
   
-  pub fn scalar(ty: Ty) -> FieldDecl {
-    FieldDecl::Scalar(ty)
-  }
-  
-  pub fn record_from_vec(fields: Vec<Field>) -> FieldDecl {
+  pub fn record_from_vec(fields: Vec<Field>) -> FieldDecl 
+  { 
     FieldDecl::Record(Record::new(fields))
   }
 }
 
-impl fmt::Display for FieldDecl {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl fmt::Display for FieldDecl 
+{
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result 
+  {
     write!(f, "{}", display_field_decl(self))
   }
 }
 
-fn display_field_decl(decl: &FieldDecl) -> String {
+fn display_field_decl(decl: &FieldDecl) -> String 
+{
   match *decl {
     FieldDecl::Scalar(ref ty)         => ty.name().to_owned(),
     FieldDecl::Record(ref r)          => format!("record ({})", r),
@@ -138,49 +151,52 @@ fn display_field_decl(decl: &FieldDecl) -> String {
 
 
 #[derive(Clone, PartialEq, Debug)]
-pub struct Record {
-  fields: Vec<Field>
-}
+pub struct Record { fields: Vec<Field> }
 
-impl Record {
-  pub fn new(fields: Vec<Field>) -> Record {
-    Record {fields: fields}
-  }
+impl Record 
+{
+  pub fn new(fields: Vec<Field>) -> Record { Record {fields: fields} }
 
-  pub fn size(&self) -> usize {
-    self.fields.len()
-  }
+  pub fn size(&self) -> usize { self.fields.len() }
 
-  pub fn find_id(&self, name: &str) -> Option<ColumnId> {
+  pub fn find_id(&self, name: &str) -> Option<ColumnId> 
+  {
     self.fields.iter().position(|f| f.name == name)
   }
 
-  pub fn find_by_name(&self, name : &str) -> Option<&Field> {
+  pub fn find_by_name(&self, name : &str) -> Option<&Field> 
+  {
     self.fields.iter().filter(|&f| f.name == name).next()
   }
 
-  pub fn iter(&self) -> Iter<Field> {
+  pub fn iter(&self) -> Iter<Field> 
+  {
     self.fields.iter()
   }
 }
 
-impl Index<ColumnId> for Record {
+impl Index<ColumnId> for Record 
+{
   type Output = Field;
-  fn index<'a>(&'a self, id: ColumnId) -> &'a Field {
+  fn index<'a>(&'a self, id: ColumnId) -> &'a Field 
+  {
     debug_assert!(id < self.fields.len(), "Field index is out of range");
     &self.fields[id]
   }
 }
 
-impl fmt::Display for Record {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl fmt::Display for Record 
+{
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result 
+  {
     write!(f, "{}", display_fields(&self.fields))
   }
 }
 
 
 #[allow(dead_code)]
-fn create_test_schema() -> Record {
+fn create_test_schema() -> Record 
+{
 	Record::new(vec![
     Field::scalar("col1", *INT4_TY),
     Field::record_from_vec("col2", vec![
@@ -203,7 +219,8 @@ fn create_test_schema() -> Record {
 }
 
 #[test]
-fn test_schema_creation() {
+fn test_schema_creation() 
+{
   let schema = create_test_schema();
   assert_eq!(vec![
     "col1 int4, col2 record (col3 int4, col4 int4), col5 record (col6 array<int8>, ",
