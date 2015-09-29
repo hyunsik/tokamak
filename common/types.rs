ed6794370ed70b62fa6e5494a4cb8d9ba2bb9178
Hyunsik Choi <hyunsik@apache.org>
2015-09-29 05:10:36 -0700

    Implement TypeManager and TypeRegistry.

--- common/types.rs
+++ common/types.rs
@@ -1,16 +1,93 @@
+use std::collections::BTreeMap;
 use std::fmt;
 use std::mem;
 
 use itertools::Itertools;
 
-use rows::{Page, MiniPage, FMiniPage};
+use rows::{MiniPage, FMiniPage};
 use str::StrSlice;
 
 // globally unique id
-#[derive(Clone, PartialEq, Debug)]
+#[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct TypeId {
   name: &'static str
-}  
+}
+
+pub trait TypeManager 
+{
+  fn get(&self, id: &TypeId) -> Option<&Type>;
+  fn types(&self) -> Vec<&Type>;
+}
+
+pub struct TypeRegistry 
+{
+  types: BTreeMap<TypeId, Box<Type>>
+}
+
+impl TypeRegistry 
+{
+  fn new(types: Vec<Box<Type>>) -> TypeRegistry 
+  {
+    TypeRegistry {
+      types: types.into_iter()
+        .map(|ty: Box<Type>| { 
+            return (*ty.id(), ty) 
+        })
+        .collect::<BTreeMap<TypeId, Box<Type>>>()    
+    }
+  }
+}
+
+impl TypeManager for TypeRegistry {
+  fn get(&self, id: &TypeId) -> Option<&Type> {
+    match self.types.get(id) {
+      Some(x) => Some(&**x),
+      None    => None
+    }
+  }
+  
+  fn types(&self) -> Vec<&Type> {
+    self.types.values().map(|t| &**t).collect::<Vec<&Type>>()
+  }
+}
+
+const BOOL_STR       : &'static str = "bool";
+const INT1_STR       : &'static str = "int1";
+const INT2_STR       : &'static str = "int2";
+const INT4_STR       : &'static str = "int4";
+const INT8_STR       : &'static str = "int8";
+const FLOAT4_STR     : &'static str = "float4";
+const FLOAT8_STR     : &'static str = "float8";
+const NUMERIC_STR    : &'static str = "numeric";
+const DATE_STR       : &'static str = "date";
+const TIME_STR       : &'static str = "time";
+const TIMEZ_STR      : &'static str = "time with timezone";
+const TIMESTAMP_STR  : &'static str = "timestamp";
+const TIMESTAMPZ_STR : &'static str = "timestamp with timezone";
+const INTERVAL_STR   : &'static str = "interval";
+const CHAR_STR       : &'static str = "char";
+const BINARY_STR     : &'static str = "binary";
+const CLOB_STR       : &'static str = "clob";
+const BLOB_STR       : &'static str = "blob";
+
+pub static BOOL:       TypeId = TypeId {name: BOOL_STR};
+pub static INT1:       TypeId = TypeId {name: INT1_STR};
+pub static INT2:       TypeId = TypeId {name: INT2_STR};
+pub static INT4:       TypeId = TypeId {name: INT4_STR};
+pub static INT8:       TypeId = TypeId {name: INT8_STR};
+pub static FLOAT4:     TypeId = TypeId {name: FLOAT4_STR};
+pub static FLOAT8:     TypeId = TypeId {name: FLOAT8_STR};
+pub static NUMERIC:    TypeId = TypeId {name: NUMERIC_STR};
+pub static DATE:       TypeId = TypeId {name: DATE_STR};
+pub static TIME:       TypeId = TypeId {name: TIME_STR};
+pub static TIMEZ:      TypeId = TypeId {name: TIMEZ_STR};
+pub static TIMESTAMP:  TypeId = TypeId {name: TIMESTAMP_STR};
+pub static TIMESTAMPZ: TypeId = TypeId {name: TIMESTAMPZ_STR};
+pub static INTERVAL:   TypeId = TypeId {name: INTERVAL_STR};
+pub static CHAR:       TypeId = TypeId {name: CHAR_STR};
+pub static BINARY:     TypeId = TypeId {name: BINARY_STR};
+pub static CLOB:       TypeId = TypeId {name: CLOB_STR};
+pub static BLOB:       TypeId = TypeId {name: BLOB_STR};
 
 pub trait HashFn<T> {
   fn hash(v: &MiniPage, keys: &mut [T]);
@@ -24,7 +101,7 @@ pub trait HashFnFactory
 
 pub trait Type 
 {
-  fn type_id           (&self) -> &TypeId;
+  fn id                (&self) -> &TypeId;
   fn display_name      (&self) -> &str;
   fn is_comparable     (&self) -> bool;
   fn is_orderable      (&self) -> bool;
@@ -36,11 +113,9 @@ pub trait Type
 #[derive(Clone, PartialEq, Debug)]
 pub struct Int4Type;
 
-pub static INT4: TypeId = TypeId {name: "int4"};
-
 impl Type for Int4Type {
   #[inline]
-  fn type_id(&self) -> &TypeId { &INT4 }
+  fn id(&self) -> &TypeId { &INT4 }
   #[inline]
   fn display_name(&self) -> &str { &INT4.name }
   #[inline]
