413c08262ca8bd00898099ab3ced2e1931369d8c
Hyunsik Choi <hyunsik@apache.org>
2015-10-17 06:35:30 -0700

    Improved skeleton code for the execution planner.

--- engine/planner.rs
+++ engine/planner.rs
@@ -1,16 +1,66 @@
 use std::marker::PhantomData;
 
 use common::err::{Error, TResult};
-use common::plan::{Plan, Visitor};
+use common::plan::*;
 use common::plugin::{PackageManager};
+use exec::Executor;
 
 pub fn create_plan(pkg_mgr: &PackageManager, plan: &Plan) -> TResult<ExecutionPlan> {
-  Err(Error::NotImplemented)
+  let mut planner = ExecutionPlanner::new();
+  walk_plan(&mut planner, plan);
+  planner.plan()
 }
 
-pub struct ExecutionPlanner;
-
-impl<'a> Visitor<'a> for ExecutionPlanner {
+pub struct ExecutionPlan {
+  stack: Vec<Box<Executor>>
 }
 
-pub struct ExecutionPlan;
+impl ExecutionPlan {
+  pub fn new() -> ExecutionPlan 
+  { 
+    ExecutionPlan {stack: Vec::new()} 
+  }
+}
+
+pub struct ExecutionPlanner {
+  plan: ExecutionPlan,
+  err: Option<Error>  
+}
+
+impl ExecutionPlanner 
+{
+  pub fn new() -> ExecutionPlanner 
+  {
+    ExecutionPlanner { 
+      plan: ExecutionPlan::new(),
+      err : None 
+    }
+  }
+  
+  pub fn plan(self) -> TResult<ExecutionPlan>
+  {
+    match self.err {
+      Some(e) => Err(e),
+      None    => Ok(self.plan)
+    }
+  } 
+}
+
+impl<'a> Visitor<'a> for ExecutionPlanner 
+{
+  fn visit_from(&mut self, ds: &DataSet) 
+  {
+  }
+
+  fn visit_head(&mut self, child: &Plan, rownum: usize) 
+  {
+    walk_plan(self, child);
+  }
+  
+  fn visit_tail(&mut self, child: &Plan, rownum: usize) 
+  {
+    walk_plan(self, child);
+  }
+}
+
+
