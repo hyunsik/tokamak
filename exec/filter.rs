2cd6978af0f702502ef4758f24c4d757b3e5c787
Hyunsik Choi <hyunsik@apache.org>
2015-11-01 22:38:10 -0800

    Add skeleton code for FilterExec.

---
+++ exec/filter.rs
@@ -0,0 +1,34 @@
+use common::types::Ty;
+use driver::DriverContext;
+use super::{Executor, ExecutorFactory};
+
+pub struct FilterExecFactory
+{
+	types: Vec<Ty>
+}
+
+impl FilterExecFactory 
+{
+	pub fn new(f: Box<ExecutorFactory>) -> FilterExecFactory
+	{
+		FilterExecFactory {
+			types: Vec::new()
+		}
+	} 
+}
+
+impl ExecutorFactory for FilterExecFactory
+{
+	fn create(&self, ctx: &DriverContext) -> Option<Box<Executor>>
+	{
+		None
+	}
+	
+	fn types(&self) -> &Vec<Ty>
+	{
+		&self.types
+	}
+}
+
+pub struct FilterExec;
+
