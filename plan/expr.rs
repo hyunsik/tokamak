8d77d0fc4638d56e33259667c603bc72a9e37984
Hyunsik Choi <hyunsik@apache.org>
2015-11-04 02:17:44 -0800

    Simplified ExprSpec and its visitors.

--- plan/expr.rs
+++ plan/expr.rs
@@ -29,26 +29,18 @@ pub enum ExprSpec {
   Or(Box<Expr>,Box<Expr>),
   Comp(CompOp, Box<Expr>, Box<Expr>),
   Arithm(ArithmOp, Box<Expr>, Box<Expr>),
-  Concat(Box<Expr>,Box<Expr>),
 
   // Functions
   Fn(Box<FnDecl>, Vec<Box<Expr>>),
 
-  // String operators or pattern matching predicates: lhs - pattern, rhs - value
-  Like(Box<Expr>,Box<Expr>, bool),
-  SimilarTo(Box<Expr>,Box<Expr>, bool),
-  RegexMatch(Box<Expr>,Box<Expr>, bool),
-
   // Other predicates
   Between(Box<Expr>,Box<Expr>,Box<Expr>), // predicand, begin, end
-  In(Box<Expr>, Vec<Box<Expr>>), // predicand, a set
 
   // condition
-  Case(Vec<Box<Expr>>, Box<Expr>), // multiple conditions, else return value
-  IfThen(Box<Expr>, Box<Expr>), // condition, return value
+  Switch(Vec<Box<Expr>>, Box<Expr>), // multiple conditions, else return value
+  Case  (Box<Expr>, Box<Expr>),      // condition, return value
 
   // values
-  Row(Vec<Box<Expr>>),
   Field(String),
   Const(Literal)
 }
@@ -147,45 +139,23 @@ pub mod visitor {
 	    }
 	  }
 	
-	  fn visit_like(&mut self, pat: &'v Expr, pred: &'v Expr, not: bool) {
-	    walk_expr(self, pat);
-	    walk_expr(self, pred);
-	  }
-	  fn visit_similarto(&mut self, pat: &'v Expr, pred: &'v Expr, not: bool) {
-	    walk_expr(self, pat);
-	    walk_expr(self, pred);
-	  }
-	  fn visit_regexmatch(&mut self, pat: &'v Expr, pred: &'v Expr, not: bool) {    
-	    walk_expr(self, pat);
-	    walk_expr(self, pred);
-	  }
-	
-	  fn visit_between(&mut self, pred: &'v Expr, begin: &'v Expr, end: &'v Expr) {
-	    walk_expr(self, pred);
+	  fn visit_between(&mut self, predicand: &'v Expr, begin: &'v Expr, end: &'v Expr) {
+	    walk_expr(self, predicand);
 	    walk_expr(self, begin);
 	    walk_expr(self, end);
 	  }
-	  fn visit_in(&mut self, pred: &'v Expr, values: &'v Vec<Box<Expr>>) {
-	    walk_expr(self, pred);
-	    self.visit_row(values);
-	  }
-	
-	  fn visit_case(&mut self, ifconds: &'v Vec<Box<Expr>>, else_result: &'v Expr) {
-	    for ref each_if in ifconds {
-	      walk_expr(self, each_if);
+	  
+	  fn visit_switch(&mut self, cases: &'v Vec<Box<Expr>>, default_value: &'v Expr) {
+	    for ref each_case in cases {
+	      walk_expr(self, each_case);
 	    }
-	    walk_expr(self, else_result);
+	    walk_expr(self, default_value);
 	  }
-	  fn visit_if_then(&mut self, ifcond: &'v Expr, result: &'v Expr) {
-	    walk_expr(self, ifcond);
+	  fn visit_case(&mut self, condition: &'v Expr, result: &'v Expr) {
+	    walk_expr(self, condition);
 	    walk_expr(self, result);
 	  }
 	
-	  fn visit_row(&mut self, values: &'v Vec<Box<Expr>>) {
-	    for ref v in values {
-	      walk_expr(self, v);
-	    }
-	  }
 	  fn visit_field(&mut self, field: &'v String) {}
 	  fn visit_const(&mut self, literal: &'v Literal) {}
 	}
@@ -195,30 +165,23 @@ pub mod visitor {
 
 	  match *expr.spec() {
 	
-	    ExprSpec::Not(ref child) => {
-	      visitor.visit_not(child);
-	    }
+	    ExprSpec::Not(ref child) => visitor.visit_not(child),
 	
 	    ExprSpec::IsNull(ref child, positive) => {
-	      visitor.visit_is_null(child, positive);
+	      visitor.visit_is_null(child, positive)
 	    }
 	
 	    ExprSpec::Sign(ref child, positive) => {
-	      visitor.visit_sign(child, positive);
+	      visitor.visit_sign(child, positive)
 	    }
 	
 	    ExprSpec::Cast(ref value, ref from, ref to) => {
 	      visitor.visit_cast(value, from, to)
-	    },
-	
-	
-	    ExprSpec::And(ref lhs, ref rhs) => {
-	      visitor.visit_and(lhs, rhs);
 	    }
 	
-	    ExprSpec::Or(ref lhs, ref rhs) => {
-	      visitor.visit_or(lhs, rhs);
-	    }
+	    ExprSpec::And(ref lhs, ref rhs) => visitor.visit_and(lhs, rhs),
+	
+	    ExprSpec::Or(ref lhs, ref rhs) => visitor.visit_or(lhs, rhs),
 	
 	    ExprSpec::Comp(ref op, ref lhs, ref rhs) => {
 	      visitor.visit_comp(op, lhs, rhs);
@@ -226,52 +189,22 @@ pub mod visitor {
 	
 	    ExprSpec::Arithm(ref op, ref lhs, ref rhs) => {
 	      visitor.visit_arithm(op, lhs, rhs);
+	    },
+		    
+	    ExprSpec::Fn(ref fd, ref args) => visitor.visit_fn(fd, args),
+	
+	    ExprSpec::Between(ref predicand, ref begin, ref end) => {
+	      visitor.visit_between(predicand, begin, end)
+	    },
+	    
+	    ExprSpec::Switch(ref cases, ref default_value) => {
+	      visitor.visit_switch(cases, default_value)
 	    }
-	
-	    ExprSpec::Concat(ref lhs, ref rhs) => {
-	      visitor.visit_concat(lhs, rhs);
-	    }
-	
-	
-	    ExprSpec::Fn(ref fd, ref args) => {
-	      visitor.visit_fn(fd, args);
+	    ExprSpec::Case(ref condition, ref result) => {
+	      visitor.visit_case(condition, result)
 	    },
 	
-	
-	    ExprSpec::Like(ref pat, ref pred, not) => {
-	      visitor.visit_like(pat, pred, not)
-	    },
-	
-	    ExprSpec::SimilarTo(ref pat, ref pred, not) => {
-	      visitor.visit_similarto(pat, pred, not)
-	    },
-	
-	    ExprSpec::RegexMatch(ref pat, ref pred, not) => {
-	      visitor.visit_regexmatch(pat, pred, not)
-	    },
-	
-	
-	    ExprSpec::Between(ref pred, ref begin, ref end) => {
-	      visitor.visit_between(pred, begin, end)
-	    }
-	
-	    ExprSpec::In(ref pred, ref values) => {
-	      visitor.visit_in(pred, values)
-	    }
-	
-	
-	    ExprSpec::Case(ref ifconds, ref else_result) => {
-	      visitor.visit_case(ifconds, else_result)
-	    }
-	
-	    ExprSpec::IfThen(ref ifcond, ref result) => {
-	      visitor.visit_if_then(ifcond, result)
-	    },
-	
-	
-	    ExprSpec::Row(ref values) => visitor.visit_row(values),
-	
-	    ExprSpec::Field(ref column) => visitor.visit_field(column),
+	    ExprSpec::Field(ref name) => visitor.visit_field(name),
 	
 	    ExprSpec::Const(ref datum) => visitor.visit_const(datum),
 	  }
@@ -284,6 +217,11 @@ pub mod visitor {
 	  
 	  fn accept_by_default(&self, e: &Expr) {
 	    match *e.spec() {
+	      ExprSpec::Not      (ref c)               => self.accept(c),
+	      ExprSpec::IsNull   (ref c, _)            => self.accept(c),
+	      ExprSpec::Sign     (ref c, _)            => self.accept(c),
+	      ExprSpec::Cast     (ref c, _, _)         => self.accept(c),
+	      
 	      ExprSpec::Const    (_)                   => {},
 	      ExprSpec::And      (ref l, ref r)        => { self.accept(l); self.accept(r) },
 	      _ => unimplemented!()
