9d4b2cb8bcd9c91f1643e9318e9d8d58ad8e665c
Hyunsik Choi <hyunsik@apache.org>
2015-11-17 18:19:53 -0800

    Add TPCH Q1 unit test for Processor.

--- exec/processor.rs
+++ exec/processor.rs
@@ -26,7 +26,7 @@ use common::rows::{
 	PageId
 };
 use common::session::Session;
-use common::types::Ty;
+use common::types::*;
 use plan::expr::*;
 use plan::expr::visitor::{accept_by_default, Visitor};
 
@@ -117,7 +117,7 @@ impl<'a> Interpreter<'a>
 	{
 	}
 	
-	pub fn Cast(&self, c: &Expr, from: &Ty, to: &Ty) 
+	pub fn Cast(&self, c: &Expr, f: &Ty, t: &Ty) 
 	{
 	}
 	
@@ -191,7 +191,7 @@ pub struct InterpreterProcessorFactory
 
 impl InterpreterProcessorFactory
 {
-	pub fn new(exprs: Vec<Box<Expr>>) -> Box<ProcessorFactory>
+	pub fn new(exprs: &Vec<Box<Expr>>) -> Box<ProcessorFactory>
 	{
 		Box::new(InterpreterProcessorFactory {
   		factories: Vec::new()
@@ -232,4 +232,96 @@ impl Processor for InterpreterProcessor
   	
   	void_ok
   }
+}
+
+#[cfg(test)]
+mod tests {
+	use common::rows::{
+		Page,
+		PageBuilder,
+		PageId
+	};
+	use common::plugin::*;
+	use common::session::Session;
+	use common::storage::{RandomTable, MemTable};
+	use common::types::*;
+	
+	use plan::expr::*;
+	use driver::DriverContext;
+	
+	use super::*;
+	
+	#[test]
+	pub fn tpch_q1() {
+		let plugin_mgr = PluginManager::new();	
+		
+		/*
+			l_orderkey long,
+			l_partkey long,
+			l_suppkey long,
+			l_linenumber int,
+			l_quantity double,
+			l_extendedprice double,
+			l_discount double,
+			l_tax double,
+			l_returnflag string,
+			l_linestatus string,
+			l_shipdate string,
+			l_commitdate string,
+			l_receiptdate string,
+			l_shipinstruct string,
+			l_shipmode string,
+			l_comment string
+		*/
+		let types: Vec<Ty> = vec![
+	    i64_ty(), // l_orderkey      bigint
+	    i64_ty(), // l_partkey       bigint
+	    i64_ty(), // l_suppkey       bigint
+	    i32_ty(), // l_linenumber    int
+	    f64_ty(), // l_quantity      double,
+	    f64_ty(), // l_extendedprice double,
+	    f64_ty(), // l_discount      double,
+	    f64_ty(), // l_tax           double,
+	    // string types are not implmeneted yet.	    
+	  ];
+
+	  let sum_disc_price = 
+	  	Mul(
+	  		&f64_ty(), Field(&f64_ty(), "l_extendedprice"), Subtract(&f64_ty(), Const(1), Field(&f64_ty(), "l_discount")));
+	  	
+  	let sum_charge = 
+	  	Mul(&f64_ty(), sum_disc_price.clone(), Plus(&f64_ty(), Const(1), Field(&f64_ty(), "l_tax")));	   
+	  
+	  
+	  let exprs = vec![
+	  	Box::new(sum_disc_price), 
+	  	Box::new(sum_charge)
+  	];
+		let factory = InterpreterProcessorFactory::new(&exprs);
+		let drv_ctx = DriverContext::new(plugin_mgr.ty_registry(), plugin_mgr.fn_registry());
+		let processor = factory.create(&drv_ctx).ok().unwrap();
+		
+		let session    = Session;
+		let mut input  = RandomTable::new(&session, &types, 1024);
+		
+		let output_tys = exprs.iter()
+											.map(|e| e.ty().clone())
+											.collect::<Vec<Ty>>();
+		let mut output = MemTable::new(&session, &output_tys, &vec!["x","y"]);
+		
+		let mut builder = PageBuilder::new(&output_tys);
+		
+		
+		loop { 
+		  let mut read_page = input.next().unwrap();
+		  
+		  if read_page.value_count() == 0 {
+		  	break;
+		  }
+		  
+		  processor.process(read_page, &mut builder);
+		  output.write(builder.build(read_page.value_count()));
+		  builder.reset();
+		}
+	}
 }
