3841fa20dab2ae1b06b181e063c97bae6edbfdce
Hyunsik Choi <hyunsik@apache.org>
2015-11-29 22:00:59 -0800

    Add to_boxed_vec util function.
     * It converts Vec<T> into Vec<Box<T>>.

--- util/collection.rs
+++ util/collection.rs
@@ -3,9 +3,20 @@
 /// Clone a vector consisting of item references to 
 /// a new vector owning cloned items.
 #[inline]
-pub fn to_owned_vec<T: Clone>(r: &Vec<&T>) -> Vec<T> {
-  r.iter().map(|v| (*v).clone()).collect::<Vec<T>>()
+pub fn to_owned_vec<T: Clone>(v: &Vec<&T>) -> Vec<T> 
+{
+  v.iter().map(|e| (*e).clone()).collect::<Vec<T>>()
 }
+
+/// Convert Vec<T> into Vec<Box<T>>
+///
+/// It is usually used for arguments of nested Struct or Enum.
+pub fn to_boxed_vec<T>(v: Vec<T>) -> Vec<Box<T>>
+{
+  v.into_iter().map(|e| Box::new(e)).collect::<Vec<Box<T>>>()
+}
+
+#[inline]
 
 #[test]
 pub fn test() {
