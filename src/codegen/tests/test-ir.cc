758a03d3e93e7b201c5d51e58d2e47c5894ebc01
Hyunsik Choi <hyunsik@apache.org>
2015-12-04 03:28:55 -0800

    Add test-ir.cc to codegen test.

---
+++ src/codegen/tests/test-ir.cc
@@ -0,0 +1,13 @@
+#include <limits.h> // MAX values
+#include <stdlib.h> // Sized Types
+#include <stdio.h>  // IO
+#include <assert.h> // Assertions
+#include <string.h> // memcpy
+
+extern "C" {
+
+double test_func1(double x) {
+  return x;
+}
+
+}
