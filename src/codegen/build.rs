7243fdb6c2678204bc5af5d985041b0ae5a87f57
Hyunsik Choi <hyunsik@apache.org>
2015-12-04 22:24:33 -0800

    Improved build.rs and test-ir.rs to generate llvm ir from rust code.

--- src/codegen/build.rs
+++ src/codegen/build.rs
@@ -11,5 +11,37 @@ fn main() {
     	.unwrap()
     	.success()
  	);
+   
+  assert!(
+  	Command::new("clang++")
+  		.args(&["tests/test-ir.cc", "-O2", "-c", "-emit-llvm", "-o", "tests/test-ir.bc"])
+    	.status()
+    	.unwrap()
+    	.success()
+ 	);
+   
+  assert!(
+  	Command::new("rustc")
+  		.args(&["test-ir-rs.rs", "--crate-type", "lib", "--emit", "llvm-ir", "-O"])
+    	.status()
+    	.unwrap()
+    	.success()
+ 	);
+   
+  assert!(
+  	Command::new("rustc")
+  		.args(&["test-ir-rs.rs", "--crate-type", "lib", "--emit", "llvm-bc", "-O"])
+    	.status()
+    	.unwrap()
+    	.success()
+ 	);
+   
+  assert!(
+  	Command::new("llvm-link")
+  		.args(&["test-ir-rs.bc", "tests/test-ir.bc", "-o=tests/test-module.bc"])
+    	.status()
+    	.unwrap()
+    	.success()
+ 	);
 
 }
