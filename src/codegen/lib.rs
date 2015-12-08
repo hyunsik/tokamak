e93e1c67d44915da892d839490fae98c4958ba6a
Hyunsik Choi <hyunsik@apache.org>
2015-12-08 11:29:24 -0800

    Add initial code of JitCompiler.

--- src/codegen/lib.rs
+++ src/codegen/lib.rs
@@ -1,15 +1,67 @@
-#![feature(libc)]
+//! JIT Compiler to generate code fragments in runtime.
 
+#![feature(libc)]
 extern crate libc;
 extern crate llvm;
 
-#[macro_use]
-extern crate util;
+extern crate common;
 
-//use llvm::*;
-//use llvm::Attribute::*;
+use std::borrow::Borrow;
+use std::marker::PhantomData;
 
-#[no_mangle]
-pub extern "C" fn test_func3(x: f64) -> f64 {
-  x
+use llvm::*;
+
+use common::err::{Void, void_ok};
+
+pub struct JitCompiler<'a> 
+{
+  ctx   : CBox<Context>,
+  module: Option<CSemiBox<'a, Module>>,
+  ee    : Option<JitEngine<'a>>,
+  
+  _marker: PhantomData<&'a ()>
 }
+
+struct JitCompilerBuilder<'a>
+{
+  ctx   : CBox<Context>,
+  module: Option<CSemiBox<'a, Module>>,
+  ee    : Option<JitEngine<'a>>,
+  _marker: PhantomData<&'a ()>
+}
+
+impl<'a> JitCompilerBuilder<'a>
+{
+  pub fn init_module(&'a mut self, name: &str) -> &'a mut JitCompilerBuilder 
+  {
+    self.module = Some(Module::new(name, self.ctx.borrow()));
+    self.ee     = Some(JitEngine::new((self.module.as_ref()).unwrap(), JitOptions {opt_level: 0}).unwrap());
+    
+    self
+  }
+}
+
+/*
+impl<'a> JitCompiler<'a>
+{
+  pub fn new() -> JitCompilerBuilder<'a>
+  {
+    JitCompiler {      
+      ctx    : Context::new(),
+      module : None,
+      ee     : None,
+      
+      _marker: PhantomData
+    }
+  }
+
+  pub fn load_from_bitcode(&'a mut self, path: &str) -> Void
+  {    
+    self.module = Some(Module::parse_bitcode(self.ctx.borrow(), path)
+      .expect(&format!("loading {} failed...", path)));
+    self.ee     = Some(JitEngine::new((self.module.as_ref()).unwrap(), JitOptions {opt_level: 0}).unwrap());
+    
+    void_ok
+  }
+}
+*/
--- src/codegen/tests/func_finder_tests.rs
+++ src/codegen/tests/func_finder_tests.rs
@@ -2,39 +2,16 @@
 
 extern crate libc;
 extern crate llvm;
-//extern crate llvm_sys;
-
-#[macro_use]
-extern crate util;
 
 extern crate codegen;
 
 use llvm::*;
 use llvm::Attribute::*;
-//use llvm_sys::core::LLVMSetLinkage;
-//use llvm_sys::LLVMLinkage;
-
 use std::mem;
 
-use codegen::test_func3;
-
-fn test_func_find(name: &str, lang: &str) {
-  let ctx = Context::new();
-  let module = Module::parse_bitcode(&ctx, "tests/test-module.bc").expect("loading test-module.bc failed...");
-  
-  //let ret_type = Type::get::<f64>(&ctx);
-  //let param = vec![Type::get::<f64>(&ctx)];
-  //let fn_ty = Type::function_ty(ret_type, &param);
-  //let f = module.add_function("test_func3", fn_ty);
-  //core::LLVMSetLinkage(f, LLVMLinkage::LLVMExternalLinkage);
-  
-  module.verify().expect("verifying the module failed...");
-  
-  for x in module.into_iter() {
-  	x.add_attribute(Attribute::AlwaysInline);
-  }
- 
-  let ee = JitEngine::new(&module, JitOptions {opt_level: 0}).unwrap();  
+fn test_func_find(module: &Module, name: &str, lang: &str) 
+{
+  let ee = JitEngine::new(module, JitOptions {opt_level: 0}).unwrap();  
   let func = ee.find_function(name).expect(&format!("find_function: couldn't find {} from {}", name, lang));
 	let f: fn(f64) -> f64;
   f = match unsafe { ee.get_function_raw(func) } {
@@ -45,36 +22,18 @@ fn test_func_find(name: &str, lang: &str) {
   assert_eq!(98.0f64, f(98.0f64));
 }
 
-//#[test]
-pub fn test_func_declaration() {
+#[test]
+pub fn test_c_func() 
+{
   let ctx = Context::new();
   let module = Module::parse_bitcode(&ctx, "tests/test-module.bc").expect("loading test-module.bc failed...");
   module.verify().expect("verifying the module failed...");
   
-  for x in module.into_iter() {
-  	x.add_attribute(Attribute::AlwaysInline);
-  }
-  
   let func = module.get_function("test_func1").unwrap();
-  assert_eq!(1, func.num_params());
-  
+  assert_eq!(1, func.num_params());  
   let param = &func[0];
   assert_eq!(Type::get::<f64>(&ctx), param.get_type());
-}
-
-//#[test]
-pub fn test_c_func() {
-  test_func_find("test_func1", "c");
-}
-
-//#[test]
-pub fn test_rust_func() {
-  test_func_find("test_func2", "rust");
-}
-
-#[test]
-pub fn test_rust_func2() {
-  println!("{}", test_func3(1980.0f64));
   
-  test_func_find("test_func3", "rust");
+  test_func_find(&module, "test_func1", "c");
+  test_func_find(&module, "test_func2", "rust");
 }
