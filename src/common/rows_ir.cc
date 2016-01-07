6bfdf38a87795fa4269ac11f86513ebccffe9b35
Hyunsik Choi <hyunsik@apache.org>
2016-01-07 02:31:26 -0800

    Add rows_ir.cc/rs.

---
+++ src/common/rows_ir.cc
@@ -0,0 +1,22 @@
+#include<cstdint>
+#include<stddef.h>
+
+struct MiniPage {
+  void *ptr;
+  size_t len;
+};
+
+struct Page {
+  MiniPage* pages;
+  size_t num;
+};
+
+extern "C" void write_i8(MiniPage* page, uint32_t idx, int8_t val) {
+  reinterpret_cast<int8_t*>(page->ptr)[idx] = val;
+}
+
+extern "C" void write_i32(MiniPage* page, uint32_t idx, int32_t val) {
+  reinterpret_cast<int32_t*>(page->ptr)[idx] = val;
+}
+
+void dummy(Page* v1, MiniPage* v2) {}
