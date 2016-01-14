0cf8dee1eb3509dce96eaf1ae66dad1151274e85
Hyunsik Choi <hyunsik@apache.org>
2016-01-14 10:22:18 -0800

    Add more APIs for raw minipage.

--- src/common/rows_ir.h
+++ src/common/rows_ir.h
@@ -8,31 +8,45 @@ enum MiniPageType {
 };
 
 struct MiniPage {
-  void* ptr;
+  // MiniPageType type; - can be used for debugging
+  void   *ptr;
+  // byte size
   size_t size;
 };
 
 struct Page {
   MiniPage* mpages;
-  // the number of mini pages  
-  size_t num;
-  // The number of values stored in each minipage.
-  // All minipages share the same value count;
-  size_t value_cnt;
-  // Does this page own the mini pages?
-  bool   owned;
+  // the number of minipages
+  size_t    mpage_num;
+  // the number of values stored in each minipage.
+  // All minipages share the same val_cnt.
+  size_t    value_cnt;
+  // Does this page own the minipages?
+  bool      owned;
 };
 
-extern "C" void write_i8(MiniPage* page, uint32_t idx, int8_t val) {
+extern "C" void write_i8_raw(MiniPage* page, uint32_t idx, int8_t val) {
   reinterpret_cast<int8_t*>(page->ptr)[idx] = val;
 }
 
-extern "C" void write_i32(MiniPage* page, uint32_t idx, int32_t val) {
+extern "C" void write_i16_raw(MiniPage* page, uint32_t idx, int16_t val) {
+  reinterpret_cast<int16_t*>(page->ptr)[idx] = val;
+}
+
+extern "C" void write_i32_raw(MiniPage* page, uint32_t idx, int32_t val) {
   reinterpret_cast<int32_t*>(page->ptr)[idx] = val;
 }
 
-extern "C" int32_t test(MiniPage* page, int32_t x) {
-  return x;
+extern "C" void write_i64_raw(MiniPage* page, uint32_t idx, int64_t val) {
+  reinterpret_cast<int64_t*>(page->ptr)[idx] = val;
+}
+
+extern "C" void write_f32_raw(MiniPage* page, uint32_t idx, float val) {
+  reinterpret_cast<float*>(page->ptr)[idx] = val;
+}
+
+extern "C" void write_f64_raw(MiniPage* page, uint32_t idx, double val) {
+  reinterpret_cast<double*>(page->ptr)[idx] = val;
 }
 
 void dummy(Page* v1, MiniPage* v2) {}
