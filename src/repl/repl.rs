107e6f958a471d1684cb5b8b11b5213437d10ff5
Hyunsik Choi <hyunsik@apache.org>
2016-01-29 17:09:08 -0800

    Trivial improvement for repl.

--- src/repl/repl.rs
+++ src/repl/repl.rs
@@ -8,8 +8,13 @@ use readline::*;
 use parser::lexer;
 use parser::parser as p;
 
+// Keep
+struct ReplContext;
+
 pub fn main() {
   unsafe {
+    let mut ast = Vec::new();
+
     loop {
       let line = readline(from_str("\x1b[33mtkm> \x1b[0m"));
       if line.is_null() {
@@ -17,14 +22,20 @@ pub fn main() {
       }
 
       let tokens = lexer::tokenize(to_str(line));
-      let ast = Vec::new();
       let parsed = p::parse(&tokens[..], &ast[..]);
 
-      for x in parsed.ok().unwrap().0 {
-        println!("{:?}", x);
+      match parsed {
+        Ok(r) => {
+          match r.1.len() {
+            0 => {
+              println!("{:?}, {:?}", r.0, r.1);
+              add_history(line);
+            }
+            _ => {}
+          }
+        }
+        Err(msg) => {println!("{}", msg)}
       }
-
-      add_history(line);
     }
   }
 }
