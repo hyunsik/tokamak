291c79134fa5e2deeebb5a6133de5f8d3788f775
Hyunsik Choi <hyunsik@apache.org>
2016-02-04 12:18:21 -0800

    Add encode_value macro and implement them for all types.

--- src/repl/value_printer.rs
+++ src/repl/value_printer.rs
@@ -1,9 +1,24 @@
 use std::fmt::Write;
+use std::convert::Into;
+use llvm::builder::Builder;
+use llvm::value::Value;
+use common::types::Ty;
 
-pub extern "C" fn encode_i8(buf: &mut String, val: i8) {
-  write!(buf, "{}", val).unwrap();
-}
+macro_rules! encode_value(
+  ($name:ident, $ty:ty) => (
+    #[allow(dead_code)]
+    pub extern "C" fn $name(buf: &mut String, val: $ty) {
+      write!(buf, "{}", val).unwrap();
+    }
+  );
+);
 
-pub extern "C" fn encode_i8(buf: &mut String, val: i8) {
-  write!(buf, "{}", val).unwrap();
+encode_value!(encode_i8, i8);
+encode_value!(encode_i16, i16);
+encode_value!(encode_i32, i32);
+encode_value!(encode_i64, i64);
+encode_value!(encode_f32, f32);
+encode_value!(encode_f64, f64);
+
+pub fn write<V: Into<Value>>(buf: &mut String, val: &Value) {
 }
