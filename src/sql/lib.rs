0c7e0d0a2378f874fdd13d11a781c7d8e1aceccf
Hyunsik Choi <hyunsik@apache.org>
2016-02-06 21:49:51 -0800

    Reformat sql module.

--- src/sql/lib.rs
+++ src/sql/lib.rs
@@ -1,18 +1,19 @@
 extern crate common;
 
-use common::err::{void_ok, Void};
-use common::plugin::{Plugin, PluginManager}; 
+use common::err::{Void, void_ok};
+use common::plugin::{Plugin, PluginManager};
 
 const PACKAGE_NAME: &'static str = "sql";
 
 pub struct SQLPackage;
 
 impl Plugin for SQLPackage {
-  fn name(&self) -> &str { PACKAGE_NAME }
-  
+  fn name(&self) -> &str {
+    PACKAGE_NAME
+  }
+
   #[allow(unused_variables)]
-  fn load(&self, mgr: &mut PluginManager) -> Void
-  {
-  	void_ok
-  } 
+  fn load(&self, mgr: &mut PluginManager) -> Void {
+    void_ok
+  }
 }
