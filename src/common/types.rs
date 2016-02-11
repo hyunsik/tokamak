a565c6484af0103ff71dc489e27b061eb2414b86
Hyunsik Choi <hyunsik@apache.org>
2016-02-11 15:16:08 -0800

    Improve common::Ty.
     * Remove commented out lines.
     * Implement Display for Ty.
     * Implement Array and Tuple types.

--- src/common/types.rs
+++ src/common/types.rs
@@ -2,99 +2,17 @@
 //! So, Types are loaded from the packages.
 //!
 
+use std::fmt;
 use std::mem;
 use std::cmp::Ordering;
 use std::rc::Rc;
+
+use itertools::Itertools;
 
 use err::Result;
 use page::Chunk;
 
 pub type TypeFactory = Rc<Fn(&str) -> Result<Ty>>;
-
-#[derive(Clone)]
-pub struct TypeHandler {
-  pub create_minipage: Rc<Fn() -> Box<Chunk>>,
-}
-
-// #[derive(Clone, Eq, PartialEq, PartialOrd, Ord)]
-// pub struct Ty {
-// base         : String,
-// comparable   : bool,
-// orderable    : bool,
-// handler      : TypeHandler
-// }
-//
-
-// impl Ty {
-// pub fn new(base: &str, comparable: bool, orderable: bool, handler:
-// TypeHandler) -> Ty {
-// Ty {
-// base      : base.to_string(),
-// comparable: comparable,
-// orderable : orderable,
-// handler   : handler
-// }
-// }
-//
-// pub fn base(&self) -> &str
-// {
-// &self.base
-// }
-//
-// pub fn handler(&self) -> &TypeHandler
-// {
-// &self.handler
-// }
-// }
-//
-// For ignoring
-// impl Eq for TypeHandler {}
-//
-// For ignoring
-// impl PartialEq for TypeHandler {
-// fn eq(&self, other: &TypeHandler) -> bool {
-// true
-// }
-// }
-//
-// For ignoring
-// impl PartialOrd for TypeHandler {
-// fn partial_cmp(&self, other: &TypeHandler) -> Option<Ordering> {
-// Some(Ordering::Equal)
-// }
-// }
-//
-// For ignoring
-// impl Ord for TypeHandler {
-// fn cmp(&self, other: &TypeHandler) -> Ordering {
-// Ordering::Equal
-// }
-// }
-
-// impl Ty {
-//  pub fn kind(&self) -> &TyKind {
-//    &self.kind
-//  }
-//
-//  pub fn handler(&self) -> Rc<TypeHandler> {
-//    match self.kind {
-//      TyKind::I32 => {
-// let f = || -> Box<MiniPage>
-// {Box::new(FMiniPage::new(mem::size_of::<f32>()))};
-//        Rc::new(TypeHandler {create_minipage: Rc::new(f)})
-//      },
-//
-//      TyKind::F32 => {
-// let f = || -> Box<MiniPage>
-// {Box::new(FMiniPage::new(mem::size_of::<i32>()))};
-//        Rc::new(TypeHandler {create_minipage: Rc::new(f)})
-//      },
-//
-//      _  => { panic!("Unknown supported type") }
-//    }
-//  }
-// }
-//
 
 pub mod name {
   pub static BOOL: &'static str = "bool";
@@ -105,6 +23,8 @@ pub mod name {
   pub static I64: &'static str = "i64";
   pub static F32: &'static str = "f32";
   pub static F64: &'static str = "f64";
+  pub static Array: &'static str = "Array";
+  pub static Tuple: &'static str = "Tuple";
 }
 
 pub static BOOL: &'static Ty = &Ty::Bool;
@@ -116,30 +36,17 @@ pub static I64: &'static Ty = &Ty::I64;
 pub static F32: &'static Ty = &Ty::F32;
 pub static F64: &'static Ty = &Ty::F64;
 
-#[macro_export]
-macro_rules! schema {
-  ( $( $t:expr ),* ) => {
-    {
-      let mut schema = Vec::new();
-        $(
-          schema.push($t.clone());
-        )*
-
-        schema
-    }
-  };
-}
-
-/// Ty
+/// Types
+///
 /// * Bool
-/// * U8:
-/// * I8:
-/// * I16:
-/// * I32:
-/// * I64:
+/// * U8 : unsigned 8 bit integer
+/// * I8 : signed 8 bit integer
+/// * I16: signed 16 bit integer
+/// * I32: signed 32 bit integer
+/// * I64: signed 64 bit integer
 /// * F32: single precision floating type
 /// * F64: double precision floating type
-/// * Vector: an ordered list of same elements
+/// * Array: multi-dimensional array of same elements
 /// * Tuple: an ordered list of arbtrary type values
 #[derive(Clone, PartialEq, PartialOrd, Eq, Ord, Debug)]
 pub enum Ty {
@@ -151,7 +58,7 @@ pub enum Ty {
   I64,
   F32,
   F64,
-  Vector(Box<Ty>),
+  Array(Box<Ty>, Vec<usize>),
   Tuple(Vec<Ty>),
 }
 
@@ -167,7 +74,7 @@ impl Ty {
       Ty::I64 => name::I64,
       Ty::F32 => name::F32,
       Ty::F64 => name::F64,
-      Ty::Vector(_) => panic!("unsupported type: vector"),
+      Ty::Array(..) => panic!("unsupported type: vector"),
       Ty::Tuple(_) => panic!("unsupported type: tuple"),
     }
   }
@@ -189,17 +96,11 @@ impl Ty {
   }
 
   pub fn is_uint(&self) -> bool {
-    match *self {
-      Ty::U8 => true,
-      _ => false,
-    }
+    *self == Ty::U8
   }
 
   pub fn is_float(&self) -> bool {
-    match *self {
-      Ty::F32 | Ty::F64 => true,
-      _ => false,
-    }
+    *self == Ty::F32 || *self == Ty::F64
   }
 
   pub fn size_of(&self) -> usize {
@@ -209,8 +110,8 @@ impl Ty {
       Ty::I16 => 2,
       Ty::I32 | Ty::F32 => 4,
       Ty::I64 | Ty::F64 => 8,
-      Ty::Vector(_) => panic!("unsupported type: vector"),
-      Ty::Tuple(_) => panic!("unsupported type: tuple"),
+      Ty::Array(ref ty, ref dims) => dims.iter().fold(0, |acc, dim| acc + (ty.size_of() * dim)),
+      Ty::Tuple(ref tys) => tys.iter().fold(0, |acc, ty| acc + ty.size_of()),
     }
   }
 
@@ -244,82 +145,33 @@ pub fn f(size: u32) -> &'static Ty {
   }
 }
 
+/// Typed implementation
 pub trait HasType {
   fn ty(&self) -> &Ty;
 }
 
-// impl Ty
-// {
-// #[allow(unused_variables)]
-// pub fn size_of(&self) -> u32 {
-// match *self {
-// Ty::Bool                  => 1,
-// Ty::Int1                  => 1,
-// Ty::Int2                  => 2,
-// Ty::Int4                  => 4,
-// Ty::Int8                  => 8,
-// Ty::Float4                => 4,
-// Ty::Float8                => 8,
-// Ty::Numeric(ref p, ref s) => panic!("numeric is not supported yet"),
-// Ty::Date                  => 4,
-// Ty::Time                  => 8,
-// Ty::Timez                 => 12,
-// Ty::Timestamp             => 8,
-// Ty::Timestampz            => 12,
-// Ty::Interval              => 12,
-// Ty::Char(len)             => len as u32,
-// Ty::Binary(len)           => len as u32,
-// Ty::Clob                  => mem::size_of::<TEXT>() as u32,
-// Ty::Blob                  => mem::size_of::<TEXT>() as u32,
-// Ty::Array(ref ty)         => panic!("array is not supported yet"),
-// Ty::Struct(ref tys)       => panic!("array is not supported yet"),
-// Ty::Map(ref kt, ref vt)   => panic!("map is not supported yet"),
-// }
-// }
-//
-// #[allow(unused_variables)]
-// pub fn is_variable(&self) -> bool {
-// match *self {
-// Ty::Char(len) | Ty::Binary(len) => true,
-// _ => false
-// }
-// }
-// }
-//
-// impl fmt::Display for Ty {
-// fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-// match *self {
-// Ty::Bool                    => write!(f, "bool"),
-// Ty::Int1                    => write!(f, "int1"),
-// Ty::Int2                    => write!(f, "int2"),
-// Ty::Int4                    => write!(f, "int4"),
-// Ty::Int8                    => write!(f, "int8"),
-// Ty::Float4                  => write!(f, "float4"),
-// Ty::Float8                  => write!(f, "float8"),
-// Ty::Numeric(ref p,ref s)    => {
-// match *s {
-// Some(ref scale) => write!(f, "numeric ({}, {})", p, scale),
-// None            => write!(f, "numeric ({})", p)
-// }
-// }
-// Ty::Date                    => write!(f, "date"),
-// Ty::Time                    => write!(f, "time"),
-// Ty::Timez                   => write!(f, "time with timezone"),
-// Ty::Timestamp               => write!(f, "timestamp"),
-// Ty::Timestampz              => write!(f, "timeztamp with timezone"),
-// Ty::Interval                => write!(f, "interval"),
-// Ty::Char(ref len)           => write!(f, "char({})", len),
-// Ty::Binary(ref len)         => write!(f, "binary({})", len),
-// Ty::Clob                    => write!(f, "clob"),
-// Ty::Blob                    => write!(f, "blob"),
-// Ty::Array(ref ty)           => write!(f, "array<{}>", ty),
-// Ty::Struct(ref tys)         => {
-// write!(f, "struct({})", tys.iter().map(|f| format!("{}", f)).join(", "))
-// },
-// Ty::Map (ref kty, ref vty)  => write!(f, "map<{},{}>", kty, vty)
-// }
-// }
-// }
+
+impl fmt::Display for Ty {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match *self {
+      Ty::Bool => write!(f, "bool"),
+      Ty::I8 => write!(f, "i8"),
+      Ty::U8 => write!(f, "u8"),
+      Ty::I16 => write!(f, "i16"),
+      Ty::I32 => write!(f, "i32"),
+      Ty::I64 => write!(f, "i64"),
+      Ty::F32 => write!(f, "f32"),
+      Ty::F64 => write!(f, "f64"),
+      Ty::Array(ref ty, ref dims) => {
+        write!(f, "Array({},{})", ty, dims.iter().map(|dim| format!("{}", dim)).join(","))
+      }
+      Ty::Tuple(ref types) => {
+        write!(f, "Tuple({})", types.iter().map(|dim| format!("{}", dim)).join(","))
+      }
+    }
+  }
+}
+
 //
 // Determine a result data type from two expression data types.
 // #[allow(unused_variables)]
