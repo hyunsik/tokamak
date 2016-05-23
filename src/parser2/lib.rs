215a948b9b00938761c3a303aaf4ce3a5ca97e80
Hyunsik Choi <hyunsik@apache.org>
2016-05-23 00:05:32 -0700

    Add ptr for Pointer structure and its trait methods.

--- src/parser2/lib.rs
+++ src/parser2/lib.rs
@@ -1,10 +1,15 @@
+#![feature(filling_drop)]
 #![feature(unicode)]
 #![feature(question_mark)]
+
+#[macro_use] extern crate bitflags;
 
 pub mod ast;
 pub mod codemap;
 pub mod interner;
 pub mod lexer;
 pub mod parser;
+pub mod ptr;
 pub mod token;
+
 
