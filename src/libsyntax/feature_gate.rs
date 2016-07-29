3c281bc45fd5880a35b5b56024f1e7954a74b9d5
Hyunsik Choi <hyunsik@apache.org>
2016-07-29 03:26:16 -0700

    Removed unused and legacy crates.

--- src/libsyntax/feature_gate.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Clone, Copy)]
-pub enum UnstableFeatures {
-    /// Hard errors for unstable features are active, as on
-    /// beta/stable channels.
-    Disallow,
-    /// Allow features to me activated, as on nightly.
-    Allow,
-}
